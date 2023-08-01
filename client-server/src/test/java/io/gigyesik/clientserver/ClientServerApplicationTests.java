390e59374f5f115c3851f352c79c75b02636dad5
gigyesik <gigyesik@gmail.com>
2023-08-02 02:29:59 +0900

    client-server 추가

---
+++ client-server/src/test/java/io/gigyesik/clientserver/ClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gigyesik.clientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
