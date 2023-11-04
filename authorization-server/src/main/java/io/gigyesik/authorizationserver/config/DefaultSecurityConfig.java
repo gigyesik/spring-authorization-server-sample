f65cc5256e4572b4df0c1e785b1a886ca81adf1c
gigyesik <gigyesik@gmail.com>
2023-11-04 17:45:54 +0900

    auth server 허용 경로에 h2-console 추가

--- authorization-server/src/main/java/io/gigyesik/authorizationserver/config/DefaultSecurityConfig.java
+++ authorization-server/src/main/java/io/gigyesik/authorizationserver/config/DefaultSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -29,14 +31,21 @@ public class DefaultSecurityConfig {
                                 .requestMatchers(
                                         new AntPathRequestMatcher("/assets/**"),
                                         new AntPathRequestMatcher("/webjars/**"),
-                                        new AntPathRequestMatcher("/login"))
+                                        new AntPathRequestMatcher("/login"),
+                                        new AntPathRequestMatcher("/h2-console/**")
+                                )
                                 .permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/login")
-                )
+//                .csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+//                .formLogin(formLogin ->
+//                        formLogin
+//                                .loginPage("/login")
+//                )
+//                .headers(header ->
+//                        header
+//                                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+//                )
                 .oauth2Login(oauth2Login ->
                         oauth2Login
                                 .loginPage("/login")
